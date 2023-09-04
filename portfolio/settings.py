2799735013741dd4c55905ad952af1c35e4c2899
Om Kanekar <omkanekar28@gmail.com>
2023-09-04 17:10:05 +0530

    added blog model and homepage

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     'jobs.apps.JobsConfig',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
