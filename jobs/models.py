65e11dac8d7fc76dd029f0632114bd9a987b8fef
omkanekar28 <omkanekar28@LAPTOP-C401J7LC.localdomain>
2023-09-14 11:33:00 +0530

    14-09-2023 commit

--- jobs/models.py
+++ jobs/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
+
 class Jobs(models.Model):
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200, default = '')
+
+    def __str__(self) -> str:
+        return self.summary
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -81,6 +81,15 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+
+    # 'default': {
+    #    'ENGINE': 'django.db.backends.postgresql',
+    #    'NAME': 'portfoliodb',
+    #    'USER': 'postgres',
+    #    'PASSWORD': 'admin1234',
+    #    'HOST': 'localhost',
+    #    'PORT': '5432',
+    # }
 }
 
 
