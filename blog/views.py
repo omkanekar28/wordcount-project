0d8ddcca02f8be68ebbad54cf6dc92abd37fe5fd
omkanekar28 <omkanekar28@LAPTOP-C401J7LC.localdomain>
2023-09-15 14:46:11 +0530

    Final commit

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
 # Create your views here.
@@ -9,4 +9,5 @@ def allblogs(request):
     return render(request, 'blog/allblogs.html', context)
 
 def detail(request, blog_id):
-    return HttpResponse('Hi')
+    detailblog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/detail.html', {'blog':detailblog})
